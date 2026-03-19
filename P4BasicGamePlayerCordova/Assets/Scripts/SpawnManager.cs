f34e2b3acee87a5305d0ebdacaeb1dcb0f27125f
757297-stack <757297@student.dusd.net>
2026-03-19 12:16:23 -0700

    needa add bonus

---
+++ P4BasicGamePlayerCordova/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 10;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SpawnRandomAnimal()
+    {
+        // Pick a random index between 0 and the total number of animals
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        // Create the animal
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
